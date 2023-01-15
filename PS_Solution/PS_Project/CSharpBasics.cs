3f53860eabebea0af95b4b6a32a653c9ea7095b4
PHC <lucidsun92@gmail.com>
2023-01-15 17:41:33 +0900

    First Commit
    프로젝트 솔루션 구성, 프로젝트 1개로 여러 PS 문제를 간편히 Test할 수 있도록 환경 구성
    런타임에 Type 생성하여 확인할 수 있도록 구성하여 매번 코드 짤 때 Main함수 겹치는 문제 없도록 구성함.

---
+++ PS_Solution/PS_Project/CSharpBasics.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PS_Project
+{
+    class PS_0 : PS
+    {
+        public override void Run()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+    class PS_1 : PS
+    {
+        // 정렬: using System.Linq; 사용
+
+        class CustomElem
+        {
+            public string _name;
+            public int _age;
+
+            public CustomElem(string name, int age)
+            {
+                _name = name;   // this.name이라 쓰면 객체 이름에 접근하게 된다. (그리고 읽기 전용이라 무시된다)
+                _age = age;
+            }
+
+            public override string ToString()
+            {
+                return "Name: " + _name + ", Age: " + _age;
+            }
+        }
+
+        public override void Run()
+        {
+            List<int> ls = new List<int>() {1,3,2,5,4 };
+
+            // 원본
+            Console.WriteLine(string.Join(", ", ls));
+            
+            // 오름차순 정렬 (자료형이 int라 i=>i 그대로 씀)
+            Console.WriteLine(string.Join(", ", ls.OrderBy(i => i).ToList()));
+
+            // 내림차순 정렬
+            Console.WriteLine(string.Join(", ", ls.OrderByDescending(i => i).ToList()));
+
+            // Custom 객체
+            List<CustomElem> cls = new List<CustomElem>();
+            cls.Add(new CustomElem("tom", 33));
+            cls.Add(new CustomElem("adam", 33));
+            cls.Add(new CustomElem("blue", 33));
+            cls.Add(new CustomElem("may", 22));
+            cls.Add(new CustomElem("zolly", 22));
+
+            // 이름 오름 차순
+            Console.WriteLine(string.Join(", ", cls.OrderBy(e => e._name).ToList()));
+            // 이름 내림 차순
+            Console.WriteLine(string.Join(", ", cls.OrderByDescending(e => e._name).ToList()));
+
+            // 이름만 모아서
+            var temp = new List<string>();
+            cls.ForEach(a => temp.Add(a._name));
+            Console.WriteLine(string.Join(", ", temp.OrderBy(a=>a)));
+        }
+    }
+}
---
+++ PS_Solution/PS_Project/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PS_Project
+{
+    public class PS
+    {   // Problem Solving 클래스
+        // PS를 상속하여 Run 함수를 구현해 Main에서 실행(한 프로젝트에서 여러 소스 코드 파일을 실행 Test가능 하도록 구성)
+        static Dictionary<string, PS> testDict = new Dictionary<string, PS>();
+
+        public PS()
+        {
+            Enroll();
+        }
+
+        public virtual void Run() { }
+
+        public static PS GetTestProgram(int index)
+        {
+            string key = "PS_Project.PS_" + index;
+            if (!testDict.ContainsKey(key))
+            {
+                Console.WriteLine("해당하는 key값이 없습니다");
+                return null;
+            }
+            return testDict[key];
+        }
+
+        void Enroll()
+        {
+            // 클래스 명을 key로 사용하기 때문에 key가 중복될 일은 없다(컴파일 에러 날 것임)
+            testDict[this.ToString()] = this;
+        }
+    }
+
+    class Program
+    {
+        const int TESTINDEX = 1;    // 0번: HelloWorld 출력 테스트
+        const int TESTCOUNT = 1000;
+
+        static void Main(string[] args)
+        {
+            // 테스트 할 class 세팅 (많아지면 인덱스 범위 조절 필요함)
+            for(int i=0; i<=TESTCOUNT; ++i)
+            {
+                var type = Type.GetType("PS_Project.PS_" + i);
+                if (type != null)
+                {
+                    var instance = Activator.CreateInstance(type) as PS;
+                }
+            }
+
+            // 테스트 수행할 Class index를 입력
+            var test = PS.GetTestProgram(TESTINDEX);
+
+            test.Run();
+        }
+    }
+}
